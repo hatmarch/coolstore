f1e199e38aa367a63d3ac2732855a34ad0bd0b54
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-03-05 01:02:03 +0000

    Add POST /products endpoint (WIP)
    Does not yet write to the database
    
    Add an http request example for testing

--- catalog-service/src/main/java/com/redhat/cloudnative/service/CatalogEndpoint.java
+++ catalog-service/src/main/java/com/redhat/cloudnative/service/CatalogEndpoint.java
@@ -2,11 +2,18 @@ package com.redhat.cloudnative.service;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.redhat.cloudnative.model.Product;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -30,4 +37,25 @@ public class CatalogEndpoint {
         return new ResponseEntity<Product>(catalogService.read(id),HttpStatus.OK);
     }
 
+    // FIXME: Secure this endpoint
+    @RequestMapping(value = "/products", method = RequestMethod.POST, produces = "application/json")
+    @CrossOrigin
+    public ResponseEntity<List<Product>> addProducts(@RequestBody @Valid List<Product> products,  BindingResult bindingResult) throws Exception {
+        BindingErrorsResponse errors = new BindingErrorsResponse();
+        HttpHeaders headers = new HttpHeaders();
+        if (bindingResult.hasErrors() || (products == null)) {
+            errors.addAllErrors(bindingResult);
+            headers.add("errors", errors.toJSON());
+            return new ResponseEntity<List<Product>>(products, headers, HttpStatus.BAD_REQUEST);
+        }
+
+        Logger logger = LoggerFactory.getLogger(CatalogEndpoint.class);
+        for (Product product : products) {
+            logger.info("Got product {}", product.toString());
+        }
+        // FIXME: Save
+        //this.userService.saveUser(user);
+
+        return new ResponseEntity<List<Product>>(products, headers, HttpStatus.CREATED);
+    }
 }
